1f3c387dcb7da0c2e26a9f4fc7e36afd1adfb4f5
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-03-09 02:06:06 +0600

    Convert in Stopwatch

--- JS/indexDev.js
+++ JS/indexDev.js
@@ -1,15 +1,57 @@
 const Start=document.getElementById('startbtn');
 const Stop=document.getElementById('stopbtn');
 const Reset=document.getElementById('resetbtn');
-const Number=document.getElementById('number');
+const MS=document.getElementById('ms');
+const Second=document.getElementById('second');
+let SecondValue=parseFloat(Second.innerText);
+const Minute=document.getElementById('miniute');
+let MinuteValue=parseFloat(Minute.innerText);
+const Hour=document.getElementById('hour');
+let HourValue=parseFloat(Hour.innerText);
 
 let SetIntervalID;
 let count=1;
 
 
 const ChangeTime=(p1)=>{
-     console.log("Value of p1(2nd) :",p1);
-     Number.innerText=count++;
+    // console.log("Value of p1(2nd) :",p1);
+     MS.innerText=count++;
+     if(count===200){
+        console.log("Second: "+SecondValue+" Data type: "+typeof SecondValue);
+        count=0;
+        SecondValue = SecondValue+1;
+        console.log(`Length of Second Value: ${SecondValue.toString().length}`)
+        if(SecondValue.toString().length<2){
+            Second.innerHTML=`0${SecondValue}`
+        }else{
+            Second.innerHTML=SecondValue;
+        }
+      
+     }
+
+     if(SecondValue===15){
+        SecondValue=0;
+        Second.innerText='00';
+        MinuteValue++;
+        if(MinuteValue.toString().length<2){
+            Minute.innerText=`0${MinuteValue}`;
+        }else{
+            Minute.innerText=MinuteValue;
+        }
+      
+     }
+
+     if(MinuteValue===5){
+        MinuteValue=0;
+        Minute.innerText='0';
+        HourValue++;
+        if(HourValue.toString().length<2){
+            Hour.innerText=`0${HourValue}`;
+        }else{
+            Hour.innerText=HourValue;
+        }
+      
+     }
 
    // Number.innerText=count++;
     // if(p1===true){
@@ -22,9 +64,9 @@ const ChangeTime=(p1)=>{
 
 
 
-const Show_Clock=(p1=false)=>{
-    console.log("Value of p1: ",p1);
-    SetIntervalID=setInterval(ChangeTime,1000,p1)
+const Show_Clock=()=>{
+  //  console.log("Value of p1: ");
+    SetIntervalID=setInterval(ChangeTime,0.1)
     // if(p1===true){
     //     setTimeout(()=>{
     //         console.log("SetInterval ID: "+SetIntervalID);
@@ -35,6 +77,7 @@ const Show_Clock=(p1=false)=>{
 }
 
 Start.addEventListener('click',()=>{
+   
 
     if(Start.classList.contains('DoStop')){
        // Start.classList.remove('DoStop');
@@ -79,4 +122,12 @@ Reset.addEventListener('click',()=>{
     clearInterval(SetIntervalID);
     Number.innerText='0';
     count=0;
+    SecondValue=0;
+    MinuteValue=0;
+    HourValue=0;
+    Second.innerText='00';
+    Minute.innerText='00';
+    Hour.innerText='00';
+    MS.innerText='00'
+
 })
